668a955d8a8bca3ef9dd500ae316a59ee93e340b
Randi Mays <randimays1024@gmail.com>
2016-06-22 14:55:53 -0500

    Updating simple simon exercise

--- public/js/archer.js
+++ public/js/archer.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
 	var game = {
 		level: 1,
@@ -13,38 +13,43 @@
 		// }
 	}
 
-	// var gameButtonsArray = $(".gamebutton");
-
+var gameButtonsArray = $("*[data-value]");
 
 function generateRandom(randomNumber){
-	var randomNumber = Math.floor(Math.random() * 5) + 0;
-	game.roundSequence.push(randomNumber);
+	for (var i = 0; i < game.level; i++) {
+		var randomNumber = Math.floor(Math.random() * 5) + 0;
+		game.roundSequence.push(randomNumber);
+	}
 	console.log(game.roundSequence);
-	lightItUp();
 }
 
+// lightItUp();
+
 function lightItUp(){
-	game.roundSequence.forEach(function(number, i){
-		if (game.roundSequence[i] == 0) {
-		 	$('*[data-value="0"]').animate({
-		 		opacity: 1.0}, 200, function() {
-		 			$(this).fadeTo(200, 0.5);
-		 		})
-		}
-		 	// console.log($('*[data-value="game.roundSequence[i]"]'));
+	game.roundSequence.forEach(function(number, index) {
+		setTimeout(function() {
+			$('[data-value="' + number + '"]').animate(
+			{"opacity": "1.0"}, 200).animate(
+			{"opacity": "0.5"}, 200);
+		}, 800 * index);
 	})
 }
-// .fadeTo("fast", 1.0)
+
+$("*[data-value]").each(function(index, button){
+	$(this).on("click", function() {
+		$(this).animate(
+			{"opacity": "1.0"}, 200).animate(
+			{"opacity": "0.5"}, 200);
+	})
+})
+
+$(document).mouseup(function(event) {
+	console.log(eventData);
+})
+
 
 generateRandom();
 lightItUp();
 
-		 	// console.log($("*[data-value="0"]"));
-			// if (randomNumber == gameButtonsArray[i].dataset.value) {
-			// 	console.log("match!");
-			// 	$(gameButtonsArray[i]).fadeIn(1000);
-		// var gameButtonsArray[i].(parseInt(.dataset.value));
 
-// });
-
-
+});
